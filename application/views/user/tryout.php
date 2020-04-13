8302181fbf6e81897668a6cb7b1f1dbe1b62ef48
WahyuSahrani <wahyusahrani@gmail.com>
2020-04-13 08:29:56 +0700

    hasil latihan membuat sistem login dengan CodeIgniter

---
+++ application/views/user/tryout.php
@@ -0,0 +1,20 @@
+
+<!-- Begin Page Content -->
+<div class="container-fluid">
+
+    <!-- Page Heading -->
+    <h1 class="h3 mb-4 text-gray-800"><?= $title; ?></h1>
+
+    <h3>Sayang sudah siap belum? Kalo sudah klik aja tombol mulai dibawah ini.</h3><br>
+    <h4>Semoga berhasil ya sayang...! I  <i class="fas fa-heart"> </i> You </h4>
+    
+        <a href="https://quizizz.com/join?gc=726071"><div class="btn btn-primary">Mulai</div></a>
+     
+
+</div> 
+<!-- /.container-fluid -->
+
+</div>
+<!-- End of Main Content -->
+
+
